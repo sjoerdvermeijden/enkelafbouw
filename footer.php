6f7b1dae3752c0df18df1d7cfb20dcc9451000cd
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-01-13 16:47:02 +0100

    Initial commit

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package enkelafbouw
+ */
+
+?>
+
+<footer id="colophon" class="footer">
+	<p>Lorem, ipsum dolor.</p>
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
