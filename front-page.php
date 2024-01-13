d68ef378625cdc51891b2b611ce1ca6eeda0b1f3
Sjoerd Vermeijden <sjoerd_v92@live.nl>
2024-01-13 16:48:04 +0100

    Adds a frontpage

---
+++ front-page.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package enkelafbouw
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+
+    Voorpagina
+
+</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
